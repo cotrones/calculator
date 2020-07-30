e3cb23023905e0c010c889cfd576e9c5d30e4b49
cotrones <tomcotroneo@gmail.com>
2020-07-29 20:16:15 -0400

    completed steps 1-3 of TOP calculator project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 console.log('Hello');
+
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+const operator = (opp, x, y) => {
+	switch (opp) {
+		case 'add':
+			return add(x, y);
+		case 'sub':
+			return sub(x, y);
+		case 'multiply':
+			return multiply(x, y);
+		case 'divide':
+			return divide(x, y);
+	}
+}
